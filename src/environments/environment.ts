393c7240f5b2d6631cce3b782f4174ccbbadfd94
Carlos Oliveira <cardevisi@gmail.com>
2024-05-28 14:04:28 -0300

    feat: create setup for envirouments (#1)

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  version: '1.0.0',
+  serverUrl: 'https://tools.texoit.com/backend-java/api/movies',
+  defaultLanguage: 'pt-BR',
+  supportedLanguages: ['pt-BR'],
+};
