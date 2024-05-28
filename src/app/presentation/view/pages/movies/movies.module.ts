2d127961516771f0681d6931a53dddc7476178ca
Carlos Oliveira <cardevisi@gmail.com>
2024-05-28 15:36:29 -0300

    feat: add a progress bar to movie pages

--- src/app/presentation/view/pages/movies/movies.module.ts
+++ src/app/presentation/view/pages/movies/movies.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 import { MoviesComponent } from './movies.component';
 import { MoviesRoutingModule } from './movies-routing.module';
 import { MatTableModule } from '@angular/material/table';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [MoviesComponent],
-  imports: [CommonModule, MoviesRoutingModule, MatTableModule],
+  imports: [
+    CommonModule,
+    MoviesRoutingModule,
+    MatTableModule,
+    MatProgressBarModule,
+  ],
 })
 export class MoviesModule {}
