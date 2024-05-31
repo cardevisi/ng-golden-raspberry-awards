80a3c3867d0ce6e89385e4d13495fa3dd820c49d
Carlos Oliveira <cardevisi@gmail.com>
2024-05-31 14:40:20 -0300

    refactor: adjust catch error - first version

--- src/app/infra/http/http-interceptor.service.ts
+++ src/app/infra/http/http-interceptor.service.ts
@@ -7,7 +7,7 @@ import {
   HttpErrorResponse,
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-// import { catchError } from 'rxjs/operators';
+import { catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
 
 @Injectable({
@@ -15,19 +15,28 @@ import { Router } from '@angular/router';
 })
 export class HttpInterceptorService implements HttpInterceptor {
   constructor(private router: Router) {}
+
+  /**
+   * todo: handle auth errors
+   * */
+
+  throwError(err: HttpErrorResponse): any {
+    if (err.status === 401) {
+      console.error('Unauthorized');
+    }
+  }
+
+  handleAuthError(err: HttpErrorResponse): Observable<any> {
+    return this.throwError(err);
+  }
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     return next
       .handle(req)
-      .pipe
-      // catchError((error: HttpErrorResponse) => {
-      //   if (error.status === 401) {
-      //     this.router.navigate(['login']);
-      //   }
-      //   return throwError(error);
-      // })
-      ();
+      .pipe(
+        catchError((error: HttpErrorResponse) => this.handleAuthError(error))
+      );
   }
 }
