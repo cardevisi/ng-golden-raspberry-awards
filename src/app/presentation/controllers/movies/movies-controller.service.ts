316d191735307edb1eb0e09415b0e2a6d7629534
Carlos Oliveira <cardevisi@gmail.com>
2024-05-30 22:58:21 -0300

    refactor: apply entities with return

--- src/app/presentation/controllers/movies/movies-controller.service.ts
+++ src/app/presentation/controllers/movies/movies-controller.service.ts
@@ -3,22 +3,30 @@ import { Observable } from 'rxjs';
 import { IMoviesController } from '../../../domain/controllers/imovies-controller';
 import { IMoviesUseCase } from '../../../domain/interfaces/usecases/movies/movies-use-case';
 import { GetMovieProps } from '../../view/shared/types/get-movie-props.types';
+import { MoviesByPageEntity } from '../../../domain/entities/movies-by-page-entity';
+import { MoviesByYearEntity } from '../../../domain/entities/movies-by-year-entity';
+import { MaxMinWinIntervalForProducersEntity } from '../../../domain/entities/max-min-win-interval-for-producers-entity';
+import { StudiosWithWinCountEntity } from '../../../domain/entities/studios-with-win-count-entity';
+import { MoviesMultiplesWinnersByYearEntity } from '../../../domain/entities/movies-multiples-winners-by-year-entity';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MovieControllerService implements IMoviesController {
   constructor(private movieUseCase: IMoviesUseCase) {}
-  getMultipleWinnersByYear(): Observable<any> {
+  getMultipleWinnersByYear(): Observable<MoviesMultiplesWinnersByYearEntity> {
     return this.movieUseCase.getMultipleWinnersByYear();
   }
-  getStudiosWithWinCount(): Observable<any> {
+  getStudiosWithWinCount(): Observable<StudiosWithWinCountEntity> {
     return this.movieUseCase.getStudiosWithWinCount();
   }
-  getMaxAndMinWinnersIntervalForProducers(): Observable<any> {
+  getMaxAndMinWinnersIntervalForProducers(): Observable<MaxMinWinIntervalForProducersEntity> {
     return this.movieUseCase.getMaxAndMinWinnersIntervalForProducers();
   }
-  getMoviesByYear(winner: boolean, year: string): Observable<any> {
+  getMoviesByYear(
+    winner: boolean,
+    year: string
+  ): Observable<MoviesByYearEntity> {
     return this.movieUseCase.getMoviesByYear(winner, year);
   }
   getMovies({
@@ -26,7 +34,7 @@ export class MovieControllerService implements IMoviesController {
     size,
     year,
     winnerStatus,
-  }: GetMovieProps): Observable<any> {
+  }: GetMovieProps): Observable<MoviesByPageEntity> {
     return this.movieUseCase.getMovies({
       page,
       size,
